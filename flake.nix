2c7476057b59f5eefaea145329248e54f289f5c0
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-21 20:16:10 +0900

    Add flake.lock for managing Nix package dependencies and update flake.nix configuration

--- flake.nix
+++ flake.nix
@@ -16,19 +16,20 @@
         ];
       };
     };
-  };
-  homeConfigurations = {
-    myHome = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = import inputs.nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true; # プロプライエタリなパッケージを許可
+
+    homeConfigurations = {
+      myHome = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true; # プロプライエタリなパッケージを許可
+        };
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./home.nix
+        ];
       };
-      extraSpecialArgs = {
-        inherit inputs;
-      };
-      modules = [
-        ./home.nix
-      ];
     };
   };
 }
