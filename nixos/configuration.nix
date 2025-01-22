50ff44eb2febc35387f7b6241f31156302b18c8a
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-22 12:08:00 +0900

    Refactor NixOS configuration by cleaning up comments, organizing settings, and enabling automatic system upgrades

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 {
   config,
   pkgs,
@@ -24,14 +20,8 @@
   };
   boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
-  networking.hostName = "aquamarine"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # Networking.
+  networking.hostName = "aquamarine";
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -40,7 +30,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "C.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "C.UTF-8";
     LC_IDENTIFICATION = "C.UTF-8";
@@ -52,7 +41,6 @@
     LC_TELEPHONE = "C.UTF-8";
     LC_TIME = "C.UTF-8";
   };
-
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
@@ -99,6 +87,7 @@
   # Enable the Cinnamon Desktop Environment.
   services.xserver.desktopManager.cinnamon.enable = true;
 
+  # Display Manager.
   services.displayManager.sddm = {
     package = pkgs.kdePackages.sddm;
     enable = true;
@@ -123,16 +112,11 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.celeste = {
@@ -151,38 +135,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
-  environment.systemPackages = with pkgs; [
-    sddm-astronaut
-  ];
-
   nix = {
+    # Flakes
     settings = {
       auto-optimise-store = true;
       experimental-features = [
@@ -190,12 +144,32 @@
         "flakes"
       ];
     };
+    # Garbage colector
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 7d";
     };
   };
+
+  # Automatic system upgrades
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
+
+  # System packages
+  system.stateVersion = "24.11";
+  environment.systemPackages = with pkgs; [
+    sddm-astronaut
+  ];
   programs = {
     git = {
       enable = true;
