b63af149a9faaa2e9f9e6202284ba7b48a98b14e
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-24 23:08:41 +0900

    Reformat imports in Nix configuration for improved readability

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,35 +1,35 @@
 {
   imports = [
-./bash
-./bottom
-./cava
-./dconf
-./eza
-./fastfetch
-./fd
-./file-roller
-./fish
-./floorp
-./fzf
-./gh
-./ghostty
-./git
-./gnome-disks
-./jq
-./kdeconnect
-./mise
-./neovim
-./noisetorch
-./peaclock
-./ripgrep
-./starship
-./steam
-./thefuck
-./topgrade
-./vscode
-./xonsh
-./yazi
-./zoxide
-./zsh
+    ./bash
+    ./bottom
+    ./cava
+    ./dconf
+    ./eza
+    ./fastfetch
+    ./fd
+    ./file-roller
+    ./fish
+    ./floorp
+    ./fzf
+    ./gh
+    ./ghostty
+    ./git
+    ./gnome-disks
+    ./jq
+    ./kdeconnect
+    ./mise
+    ./neovim
+    ./noisetorch
+    ./peaclock
+    ./ripgrep
+    ./starship
+    ./steam
+    ./thefuck
+    ./topgrade
+    ./vscode
+    ./xonsh
+    ./yazi
+    ./zoxide
+    ./zsh
   ];
 }
