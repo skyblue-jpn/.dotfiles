c5dae387f52bc9dd794cf0ad49d97db5a42d5c6e
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-24 22:44:42 +0900

    Remove Steam configuration from Nix setup

--- home/programs/steam/default.nix
+++ home/programs/steam/default.nix
@@ -1,10 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-  };
+
 }
