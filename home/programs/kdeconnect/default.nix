d207fe44e3a1c99b14919462843dd64fc8265d63
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-25 13:35:19 +0900

    Add firewall configuration for KDE Connect and remove unused tools from mise config

--- home/programs/kdeconnect/default.nix
+++ home/programs/kdeconnect/default.nix
@@ -4,4 +4,9 @@
   services.kdeconnect = {
     enable = true;
   };
+
+  networking.firewall = rec {
+    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
 }
