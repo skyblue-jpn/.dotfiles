1df3557137c655a7c73602076a710ab5fd57eecf
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-25 16:27:24 +0900

    Replace poly-dark GRUB theme derivation with installation script using wget

--- nixos/boot/grub/default.nix
+++ nixos/boot/grub/default.nix
@@ -8,18 +8,9 @@
     useOSProber = true;
   };
 
-  packages.default = pkgs.stdenv.mkDerivation {
-    name = "poly-dark-grub-theme";
-    src = pkgs.fetchFromGitHub {
-      owner = "shvchk";
-      repo = "poly-dark";
-      rev = "a3143398a0f3c63b96a79858e10aa403f8fc5152";
-      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-    };
-    installPhase = ''
-      theme_dir=$out/boot/grub/themes/poly-dark
-      mkdir -p $theme_dir
-      cp -r $src/* $theme_dir/
-    '';
-  };
+  environment.systemPackages = with pkgs; [
+    (pkgs.runCommand "install-poly-dark-grub-theme" {} ''
+      wget -O - https://github.com/shvchk/poly-dark/raw/master/install.sh | bash
+    '')
+  ];
 }
