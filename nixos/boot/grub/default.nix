3ccbd769846b06fefdd7005e4c0b59c7535b8049
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-25 16:45:33 +0900

    Integrate distro-grub-themes into NixOS configuration and update flake.lock

--- nixos/boot/grub/default.nix
+++ nixos/boot/grub/default.nix
@@ -6,10 +6,16 @@
     device = "nodev";
     efiSupport = true;
     useOSProber = true;
-  };
-  inputs.distro-grub-themes.nixosModules.${system}.default
-  distro-grub-themes = {
-    enable = true;
-    theme = "NixOS";
+    theme = pkgs.stdenv.mkDerivation {
+      pname = "distro-grub-themes";
+      version = "3.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "AdisonCavani";
+        repo = "distro-grub-themes";
+        rev = "v3.1";
+        hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+      };
+      installPhase = "cp -r customize/nixos $out";
+    };
   };
 }
