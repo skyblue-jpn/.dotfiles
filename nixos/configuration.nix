13e43ecc236177d63579fc68cbed32c8b659fb85
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-25 13:40:55 +0900

    Add firewall configuration for KDE Connect to NixOS configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,6 +24,11 @@
   # Networking.
   networking.hostName = "aquamarine";
   networking.networkmanager.enable = true;
+  networking.nftables.enable = true;
+  networking.firewall = rec {
+    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
 
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
