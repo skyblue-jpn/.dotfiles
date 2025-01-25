135a555e619678a3b67d15ba9d08cd02af64be48
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-25 13:57:37 +0900

    Refactor NixOS configuration by moving system packages to a new default.nix file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,6 +9,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ./packages
     #    ../hosts/desktop
   ];
 
@@ -167,40 +168,7 @@
   #    randomizedDelaySec = "45min";
   #  };
 
-  # System packages
-  system.stateVersion = "24.11";
-  environment.systemPackages = with pkgs; [
-    sddm-astronaut
-  ];
-  programs = {
-    fish = {
-      enable = true;
-    };
-    xonsh = {
-      enable = true;
-    };
-    kdeconnect = {
-      enable = true;
-    };
-    noisetorch = {
-      enable = true;
-    };
-    gnome-disks = {
-      enable = true;
-    };
-    file-roller = {
-      enable = true;
-    };
-    dconf = {
-      enable = true;
-    };
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-    };
-  };
+
 
   # System environment variables
   environment.sessionVariables = {
