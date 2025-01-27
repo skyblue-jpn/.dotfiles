c615ac858d7202e2a3b718ba23e906df3816c238
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-27 17:12:18 +0900

    Comment out Rust version selection in mise configuration for future reference

--- home/programs/mise/default.nix
+++ home/programs/mise/default.nix
@@ -6,26 +6,26 @@
 }:
 
 #let
-  # Import the rust overlay and select the Rust version
+# Import the rust overlay and select the Rust version
 #  rustOverlayPkgs = import inputs.nixpkgs {
 #    overlays = [ (import inputs.rust-overlay) ];
 #    system = pkgs.system;
 #  };
 
-  # Use Rust latest
+# Use Rust latest
 #  rustlatest = rustOverlayPkgs.rust-bin.stable.latest.default;
-in
+#in
 {
   programs.mise = {
     enable = true;
-#    package = inputs.mise-flake.packages.${pkgs.system}.mise.overrideAttrs (old: {
-#      buildInputs = (old.buildInputs or [ ]) ++ [ rustlatest ];
+    #    package = inputs.mise-flake.packages.${pkgs.system}.mise.overrideAttrs (old: {
+    #      buildInputs = (old.buildInputs or [ ]) ++ [ rustlatest ];
 
-      # Ensure cargo uses the correct Rust version
-#      CARGO_HOME = "${rustlatest}/.cargo";
-#      RUSTC = "${rustlatest}/bin/rustc";
-#      RUSTDOC = "${rustlatest}/bin/rustdoc";
-#    });
+    # Ensure cargo uses the correct Rust version
+    #      CARGO_HOME = "${rustlatest}/.cargo";
+    #      RUSTC = "${rustlatest}/bin/rustc";
+    #      RUSTDOC = "${rustlatest}/bin/rustdoc";
+    #    });
   };
 
   home.file.".config/mise/config.toml" = {
