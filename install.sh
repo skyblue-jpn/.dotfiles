b13dad5588250fbbcffca4863377aafcd30954dc
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-27 15:15:41 +0900

    Enhance install script with error handling and detailed logging for flake update, NixOS rebuild, Home Manager switch, and garbage collection

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-# エラーが発生した場合にスクリプトを停止
-set -e || exit
+# Stop the script if an error occurs, if an undefined variable is used, or if a command in a pipeline fails
+set -euxo pipefail
 
-# スクリプトが存在するディレクトリに移動
+# Move to the directory where the script exists
 cd "$(dirname "$0")"
 
-# フレークを更新
+# Update the flake
 nix flake update
+echo "Flake update completed."
 
-# コマンドを実行
+# Rebuild and switch to the new NixOS configuration, installing the bootloader
 sudo nixos-rebuild switch --flake .#myNixOS --install-bootloader
+echo "NixOS rebuild completed."
+
+# Switch to the new Home Manager configuration
 nix run nixpkgs#home-manager -- switch --flake .#myHome
+echo "Home Manager switch completed."
+
+# Perform garbage collection on the Nix store
 sudo nix store gc
+echo "Nix store garbage collection completed."
