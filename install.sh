e186a5e9792ab8e1ee83f6168b416d9dbff28b66
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-27 14:45:44 +0900

    Update install script to include flake update before rebuilding NixOS

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ set -e || exit
 # スクリプトが存在するディレクトリに移動
 cd "$(dirname "$0")"
 
+# フレークを更新
+nix flake update
+
 # コマンドを実行
 sudo nixos-rebuild switch --flake .#myNixOS --install-bootloader
 nix run nixpkgs#home-manager -- switch --flake .#myHome
