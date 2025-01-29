25032d1b3dd20ec709094a53b4b5a555192ddfc3
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-30 01:43:07 +0900

    Add additional system packages to NixOS configuration

--- nixos/packages/default.nix
+++ nixos/packages/default.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    pkgs.kdePackages.qtstyleplugin-kvantum
+  environment.systemPackages = with pkgs; [
+    kdePackages.qtstyleplugin-kvantum
+    gtk-engine-murrine
+    sassc
+    gnome-themes-extra
   ];
 }
