283316bea45930ac692e46f9a40fd1b2267e0572
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-30 01:00:24 +0900

    Update Catppuccin theme configuration to use 'macchiato' flavor for various components

--- nixos/themes/catppuccin/default.nix
+++ nixos/themes/catppuccin/default.nix
@@ -5,13 +5,13 @@
     enable = true;
     accent = "mauve";
     flavor = "macchiato";
-    grub.flavor = "catppuccin.flavor";
-    plymouth.flavor = "catppuccin.flavor";
+    grub.flavor = "macchiato";
+    plymouth.flavor = "macchiato";
     sddm.assertQt6Sddm = true;
-    sddm.flavor = "catppuccin.flavor";
+    sddm.flavor = "macchiato";
     sddm.font = "Noto Sans CJK JP";
     sddm.fontSize = "9";
     sddm.loginBackground = true;
-    tty.flavor = "catppuccin.flavor";
+    tty.flavor = "macchiato";
   };
 }
