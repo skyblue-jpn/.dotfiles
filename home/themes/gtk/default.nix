581f293f8c7fc30d4c7ec995031418ff49c24f82
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-30 02:04:06 +0900

    Add GTK theme configuration for Catppuccin-Macchiato

---
+++ home/themes/gtk/default.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "mauve" ];
+        size = "compact";
+        tweaks = [
+          "rimless"
+          "black"
+        ];
+        variant = "macchiato";
+      };
+    };
+  };
+
+  # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
+  xdg.configFile = {
+    "gtk-4.0/assets".source =
+      "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source =
+      "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source =
+      "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
+}
