cecd19ec2d18a0402a3ef766920ef5ade4f788a8
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-29 17:13:53 +0900

    Switch kernelPackages to cachyos-rc for updated kernel support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -17,7 +17,7 @@
     ./users
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  boot.kernelPackages = pkgs.linuxPackages_cachyos-rc;
 
   # Networking.
   networking.hostName = "aquamarine";
