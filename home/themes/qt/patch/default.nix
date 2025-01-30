b6e867f17a5a537128c859f440c4e81119f51285
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-30 16:42:33 +0900

    Fix Kvantum theme source paths for Catppuccin Macchiato Mauve theme

--- home/themes/qt/patch/default.nix
+++ home/themes/qt/patch/default.nix
@@ -3,7 +3,8 @@
 {
   xdg.configFile = {
     "Kvantum/catppuccin-macchiato-mauve/catppuccin-macchiato-mauve.kvconfig".source =
-      ./kvantum.kvconfig;
-    "Kvantum/catppuccin-macchiato-mauve/catppuccin-macchiato-mauve.svg".source = ./kvantum.svg;
+      ./catppuccin-macchiato-mauve.kvconfig;
+    "Kvantum/catppuccin-macchiato-mauve/catppuccin-macchiato-mauve.svg".source =
+      ./catppuccin-macchiato-mauve.svg;
   };
 }
