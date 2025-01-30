b8013c872f9c420e77c44cdb75529f475a0106a9
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-01-30 16:47:47 +0900

    Fix Kvantum theming patch for Qt5 to resolve automatic update issues

--- home/themes/qt/patch/default.nix
+++ home/themes/qt/patch/default.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+# This is a patch to fix the broken Kvantum theming on qt5.
+# The theming file was already downloaded from the repository so it could not update automatically.
+
 {
   xdg.configFile = {
     "Kvantum/catppuccin-macchiato-mauve/catppuccin-macchiato-mauve.kvconfig".source =
