3e485545aa5d431ad6e3500464c8179759c89834
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-02-01 01:08:37 +0900

    Add nixos-artwork package derivation for dynamic wallpaper and logo installation

--- home/themes/nixos-artwork/default.nix
+++ home/themes/nixos-artwork/default.nix
@@ -1,7 +1,26 @@
 { config, pkgs, ... }:
 
+let
+  nixos-artwork = pkgs.stdenv.mkDerivation {
+    pname = "nixos-artwork-git";
+    version = "latest";
+    src = pkgs.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixos-artwork";
+      rev = "main";
+      sha256 = "";
+    };
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r $src/logo $out/
+      cp -r $src/wallpapers $out/
+    '';
+  };
+in
+
 {
-  home.packages = with pkgs.nixos-artwork; [
-    wallpapers.catppuccin-macchiato
-  ];
+  home.file = {
+    "nixos-artwork".source = nixos-artwork;
+  };
 }
