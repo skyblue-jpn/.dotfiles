d1f0f8201a77067b30ff231d809f0373e693a1cb
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-02-01 15:40:49 +0900

    Refactor mise package configuration to remove unused Rust overlay code

--- home/programs/mise/default.nix
+++ home/programs/mise/default.nix
@@ -5,27 +5,10 @@
   ...
 }:
 
-#let
-# Import the rust overlay and select the Rust version
-#  rustOverlayPkgs = import inputs.nixpkgs {
-#    overlays = [ (import inputs.rust-overlay) ];
-#    system = pkgs.system;
-#  };
-
-# Use Rust latest
-#  rustlatest = rustOverlayPkgs.rust-bin.stable.latest.default;
-#in
 {
   programs.mise = {
     enable = true;
-    #    package = inputs.mise-flake.packages.${pkgs.system}.mise.overrideAttrs (old: {
-    #      buildInputs = (old.buildInputs or [ ]) ++ [ rustlatest ];
-
-    # Ensure cargo uses the correct Rust version
-    #      CARGO_HOME = "${rustlatest}/.cargo";
-    #      RUSTC = "${rustlatest}/bin/rustc";
-    #      RUSTDOC = "${rustlatest}/bin/rustdoc";
-    #    });
+    package = inputs.mise-flake.packages.${pkgs.system}.mise;
   };
 
   xdg.configFile = {
