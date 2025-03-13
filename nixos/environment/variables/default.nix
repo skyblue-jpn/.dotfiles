1aeebf9cc097567b6fb61714c36e20d9288e2b08
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-13 22:28:40 +0900

    Add nvim as default editor in session variables

--- nixos/environment/variables/default.nix
+++ nixos/environment/variables/default.nix
@@ -2,6 +2,7 @@
   environment.sessionVariables = {
     BROWSER = "floorp";
     TERMINAL = "ghostty";
+    EDITOR = "nvim";
     XMODIFIERS = "@im=fcitx";
     GTK_IM_MODULE = "fcitx";
     QT_IM_MODULE = "fcitx";
