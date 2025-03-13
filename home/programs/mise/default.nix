f1a0a97013ab3e1459bb1cc393cde76bede08b91
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-13 22:39:53 +0900

    Add nightly Rust to mise build inputs in default.nix

--- home/programs/mise/default.nix
+++ home/programs/mise/default.nix
@@ -5,10 +5,18 @@
   ...
 }:
 
+let
+  nightlyRust = pkgs.rust-overlay.latest.nightly.default;
+in
 {
   programs.mise = {
     enable = true;
-    package = inputs.mise-flake.packages.${pkgs.system}.mise;
+    package = inputs.mise-flake.packages.${pkgs.system}.mise.overrideAttrs (oldAttrs: rec {
+      buildInputs = oldAttrs.buildInputs or [ ] ++ [
+        nightlyRust.rustc
+        nightlyRust.cargo
+      ];
+    });
   };
 
   xdg.configFile = {
