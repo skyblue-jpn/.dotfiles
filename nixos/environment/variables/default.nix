9e7e6013d79f822df06ad6e9c773cec3a237b810
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-13 22:15:01 +0900

    Add input method configuration for fcitx in default.nix

--- nixos/environment/variables/default.nix
+++ nixos/environment/variables/default.nix
@@ -2,5 +2,9 @@
   environment.sessionVariables = {
     BROWSER = "floorp";
     TERMINAL = "ghostty";
+    XMODIFIERS = "@im=fcitx";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    INPUT_METHOD = "fcitx";
   };
 }
