aaf7a0ea87cc0b019e2ce543219db8d9826cab2a
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-14 18:33:28 +0900

    Rename hardware.opengl to hardware.graphics and update 32-bit support option

--- nixos/desktop/hyprland/default.nix
+++ nixos/desktop/hyprland/default.nix
@@ -9,11 +9,11 @@ let
   pkgs-unstable = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 in
 {
-  hardware.opengl = {
+  hardware.graphics = {
     package = pkgs-unstable.mesa.drivers;
 
     # if you also want 32-bit support (e.g for Steam)
-    driSupport32Bit = true;
+    enable32Bit = true;
     package32 = pkgs-unstable.pkgsi686Linux.mesa.drivers;
   };
 
