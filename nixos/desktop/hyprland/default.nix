b0b7391d4b44184d9d206d1235da2a21016745cd
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-14 20:25:17 +0900

    Add Nix settings for Hyprland cache and trusted keys

--- nixos/desktop/hyprland/default.nix
+++ nixos/desktop/hyprland/default.nix
@@ -25,4 +25,9 @@ in
     portalPackage =
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
+
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
 }
