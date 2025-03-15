c868c4568f8b9d64ecd52b5822c363e23260429f
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-15 12:35:30 +0900

    Add style.css for Waybar customization and update default.nix configuration

--- home/desktop/hypr/utilities/waybar/default.nix
+++ home/desktop/hypr/utilities/waybar/default.nix
@@ -3,6 +3,7 @@
 {
   xdg.configFile = {
     "waybar/config".source = ./config;
+    "waybar/style.css".source = ./style.css;
   };
   home.packages = with pkgs; [
     nerd-fonts.fantasque-sans-mono
