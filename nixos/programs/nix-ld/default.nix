729d36b3ec48321cc67303a2aba11ddf624fa938
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-16 19:45:50 +0900

    Refactor default.nix: enable nix-ld and configure library dependencies

--- nixos/programs/nix-ld/default.nix
+++ nixos/programs/nix-ld/default.nix
@@ -1,5 +1,26 @@
 { config, pkgs, ... }:
-
 {
+  # Enable nix ld
   programs.nix-ld.enable = true;
+
+  # Sets up all the libraries to load
+  programs.nix-ld.libraries = with pkgs; [
+    bzip2
+    curl
+    expat
+    fuse3
+    gdbm
+    icu
+    libffi
+    libgcc
+    ncurses
+    nss
+    openssl
+    readline
+    sqlite
+    stdenv
+    tclPackages.tk
+    xz
+    zlib
+  ];
 }
