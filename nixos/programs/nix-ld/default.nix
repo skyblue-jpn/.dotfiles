3f41f9b6bd6e7a6a3bf5bc70a18b3b8760a271d6
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-17 16:03:03 +0900

    Refactor default.nix and nix-ld configuration: add additional development packages and clean up unused definitions

--- nixos/programs/nix-ld/default.nix
+++ nixos/programs/nix-ld/default.nix
@@ -5,32 +5,6 @@
   ...
 }:
 
-let
-  devPackages = with pkgs; [
-    bzip2
-    cmake
-    curl
-    expat
-    fuse3
-    gcc
-    gdbm
-    git
-    gnumake
-    icu
-    libffi
-    libgcc
-    ncurses
-    nodejs
-    nss
-    openssl
-    readline
-    sqlite
-    stdenv
-    tclPackages.tk
-    xz
-    zlib
-  ];
-in
 {
   programs.nix-ld.dev.enable = true;
 }
