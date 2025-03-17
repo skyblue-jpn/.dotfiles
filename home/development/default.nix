2c6993fdd6253891209340fdcab3353c9735dd7c
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-17 16:14:49 +0900

    Refactor default.nix and nix-ld configuration: remove unused packages and define development shell with library path

--- home/development/default.nix
+++ home/development/default.nix
@@ -4,25 +4,5 @@
   imports = [ ./nix-alien ];
   home.packages = with pkgs; [
     dotnet-sdk_9
-    bzip2
-    curl
-    expat
-    fuse3
-    gdbm
-    icu
-    libffi
-    libgcc
-    ncurses
-    nss
-    openssl
-    readline
-    sqlite
-    stdenv
-    tclPackages.tk
-    xz
-    zlib
   ];
-
-  # If installed tools are not working, add the package here.
-  home.sessionVariables.NIX_LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [ openssl ]);
 }
--- nixos/programs/nix-ld/default.nix
+++ nixos/programs/nix-ld/default.nix
@@ -7,4 +7,28 @@
 
 {
   programs.nix-ld.dev.enable = true;
+  devShells.default = pkgs.mkShell {
+    NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
+      with pkgs;
+      [
+        bzip2
+        curl
+        expat
+        fuse3
+        gdbm
+        icu
+        libffi
+        libgcc
+        ncurses
+        nss
+        openssl
+        readline
+        sqlite
+        stdenv
+        tclPackages.tk
+        xz
+        zlib
+      ]
+    );
+  };
 }
