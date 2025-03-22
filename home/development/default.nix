f25359d2feaaf62a831f5c4cd3736b1d62d03e2f
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-22 17:56:13 +0900

    remove dev packages

--- home/development/default.nix
+++ home/development/default.nix
@@ -8,16 +8,7 @@
 {
   imports = [ ./nix-alien ];
   home.packages = with pkgs; [
-    curl
     dotnet-sdk_9
-    fontconfig
-    freetype
-    gcc
-    gnumake
     inputs.fix-python.packages.${pkgs.system}.default
-    libGL
-    libxml2
-    openssl
-    pkg-config
   ];
 }
