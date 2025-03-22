cd8049d95226675ea08982cf855796d76404eaa9
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-22 13:18:32 +0900

    removed mise-builder

--- home/programs/mise/default.nix
+++ home/programs/mise/default.nix
@@ -18,10 +18,4 @@
     "mise/config.toml".source = ./config.toml;
     "fish/completions/mise.fish".source = ./mise.fish;
   };
-
-  home.file = {
-    "mise-builder/flake.nix".source = ./mise-builder/flake.nix;
-    "mise-builder/mise.toml".source = ./mise-builder/mise.toml;
-    "mise-builder/README.md".source = ./mise-builder/README.md;
-  };
 }
