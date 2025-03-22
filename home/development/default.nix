43886cec37acf1c17309c280d75e9d13467f7afd
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-22 17:08:24 +0900

    add dev packages

--- home/development/default.nix
+++ home/development/default.nix
@@ -9,6 +9,9 @@
   imports = [ ./nix-alien ];
   home.packages = with pkgs; [
     dotnet-sdk_9
+    fontconfig
+    freetype
+    gnumake
     inputs.fix-python.packages.${pkgs.system}.default
   ];
 }
