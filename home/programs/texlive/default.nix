801a4242b384c700584b8051fca8d010c23c1574
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-23 12:29:44 +0900

    add texlive scheme-full

--- home/programs/texlive/default.nix
+++ home/programs/texlive/default.nix
@@ -3,14 +3,7 @@ let
   tex = (
     pkgs.texlive.combine {
       inherit (pkgs.texlive)
-        scheme-basic
-        dvisvgm
-        dvipng # for preview and export as html
-        wrapfig
-        amsmath
-        ulem
-        hyperref
-        capt-of
+        scheme-full
         ;
     }
   );
