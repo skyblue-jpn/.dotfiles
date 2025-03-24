f4d7658f37b7f765bb4a59f952cede0a1d06ac10
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-03-24 11:11:34 +0900

    add virtualbox

--- nixos/programs/virtualbox/default.nix
+++ nixos/programs/virtualbox/default.nix
@@ -4,7 +4,6 @@
   virtualisation.virtualbox = {
     host.enable = true;
     host.enableExtensionPack = true;
-    guest.enable = true;
-    guest.dragAndDrop = true;
   };
+  users.extraGroups.vboxusers.members = [ "celeste" ];
 }
