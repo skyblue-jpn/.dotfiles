75c9182ce90fe27156e95c58410e2acdf7bf50c8
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-04-08 13:09:35 +0900

    disable unknowns

--- flake.nix
+++ flake.nix
@@ -149,10 +149,6 @@
             ];
           };
         };
-        nixConfig = {
-          extra-substituters = [ "https://yazi.cachix.org" ];
-          extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
-        };
       };
     };
 }
