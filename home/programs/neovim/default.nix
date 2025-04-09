318f7ab5fd484ed3090f3507552041b82ae1698b
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-04-09 17:16:41 +0900

    configure nixvim

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -38,10 +38,67 @@
           };
         };
       };
-      autoLoad = true;
     };
     plugins = {
+      lazy.enable = true;
+      bufferline.enable = true;
+      colorful-menu.enable = true;
       lualine.enable = true;
+      noice.enable = true;
+      mini = {
+        enable = true;
+        modules.icons = { };
+        mockDevIcons = true;
+      };
+      nui.enable = true;
+      snacks.enable = true;
+      ts-comments.enable = true;
+      lazydev.enable = true;
+      grug-far.enable = true;
+      flash.enable = true;
+      which-key.enable = true;
+      gitsigns.enable = true;
+      trouble.enable = true;
+      todo-comments.enable = true;
+      conform-nvim.enable = true;
+      lsp.enable = true;
+      lsp-format.enable = true;
+      none-ls = {
+        enable = true;
+        sources.completion = {
+          luasnip.enable = true;
+          spell.enable = true;
+          tags.enable = true;
+        };
+        sources.diagnostics = {
+          markdownlint.enable = true;
+        };
+        sources.formatting = {
+          nixfmt.enable = true;
+          nixfmt.package = pkgs.nixfmt-rfc-style;
+          prettierd.enable = true;
+        };
+      };
+      treesitter = {
+        enable = true;
+        grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+          bash
+          json
+          lua
+          make
+          markdown
+          nix
+          regex
+          toml
+          vim
+          vimdoc
+          xml
+          yaml
+        ];
+      };
+      persistence.enable = true;
+      yazi.enable = true;
+      telescope.enable = true;
     };
   };
 }
