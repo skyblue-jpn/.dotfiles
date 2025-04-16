291777e5caffeabd686c07e65fd76cdedf271277
skyblue <82032866+skyblue-jpn@users.noreply.github.com>
2025-04-16 14:18:15 +0900

    Update default.nix

--- nixos/system/default.nix
+++ nixos/system/default.nix
@@ -98,12 +98,12 @@
   # Flatpak settings (package is managed by home-manager)
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  services.flatpak.enable = true;
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
+ # services.flatpak.enable = true;
+ # systemd.services.flatpak-repo = {
+   # wantedBy = [ "multi-user.target" ];
+   # path = [ pkgs.flatpak ];
+   # script = ''
+     # flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+   # '';
+ # };
 }
