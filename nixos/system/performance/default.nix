10c3bc61164603123c3851be746e7b949e1095c7
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-05-10 10:12:47 +0900

    Reapply "Enable systemd.oomd with forced settings for improved resource management"
    This reverts commit 3eb4801064b09a85f2ad6b54e83158d06380edfd.

--- nixos/system/performance/default.nix
+++ nixos/system/performance/default.nix
@@ -12,10 +12,10 @@
     # Fedora enables these options by default. See the 10-oomd-* files here:
     # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
     systemd.oomd = {
-      enable = lib.mkForce true;
-      enableRootSlice = gDefault true;
-      enableUserSlices = gDefault true;
-      enableSystemSlice = gDefault true;
+      enable = true;
+      enableRootSlice = true;
+      enableUserSlices = true;
+      enableSystemSlice = true;
       extraConfig = {
         "DefaultMemoryPressureDurationSec" = "20s";
       };
