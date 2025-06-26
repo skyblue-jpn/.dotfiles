c12148935277da3b0818df1351f24c5e612ab3f5
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2025-06-26 15:07:56 +0900

    Remove echo-server container from quadlet configuration due to incomplete setup

--- home/development/podman/default.nix
+++ home/development/podman/default.nix
@@ -7,18 +7,7 @@
   virtualisation.quadlet = {
     autoUpdate.enable = true;
     containers = {
-      echo-server = {
-        autoStart = true;
-        serviceConfig = {
-          RestartSec = "10";
-          Restart = "always";
-        };
-        #containerConfig = {
-        # image = "docker.io/mendhak/http-https-echo:31";
-        # publishPorts = [ "127.0.0.1:8080:8080" ];
-        # userns = "keep-id";
-        # };
-      };
+      # echo-server container removed due to incomplete configuration
     };
   };
 }
