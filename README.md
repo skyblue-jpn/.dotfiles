3dd6566c87facb0b101d5dfe4eb7d915e00c0751
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2026-09-12 00:33:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Dotfiles
+
+## Config Environments
+
+### Home
+
+- Distro: Cachy OS
+- Color scheme: Catppuccin Macchiato
+
+### Work
+
+- Distro: WSL2 (Ubuntu)
+- Color scheme: Tokyo Night Night
+
+## Install
+
+```
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/skyblue-jpn/.dotfiles/main/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -euo pipefail
+
+write_miserc() {
+  sudo mkdir -p /etc/mise
+
+  sudo tee /etc/mise/miserc.toml >/dev/null <<EOF
+env = ["$env"]
+env_conf_d = true
+EOF
+}
+
+setup_home() {
+  sudo pacman -Syyu
+  sudo pacman -S --noconfirm --needed extra/mise fish
+}
+
+setup_work() {
+  sudo add-apt-repository -y ppa:fish-shell/release-4
+  sudo add-apt-repository -y ppa:jdxcode/mise
+  sudo apt update
+  sudo apt install -y mise fish
+}
+
+echo "Select Your Config Environment:"
+
+PS3="> "
+
+select env in home work; do
+  if [ -z "$env" ]; then
+    echo "Invalid selection. Exiting."
+    exit 1
+  fi
+
+  write_miserc
+
+  case "$env" in
+  home)
+    setup_home
+    ;;
+  work)
+    setup_work
+    ;;
+  esac
+
+  mise -E "$env" bootstrap --adopt skyblue-jpn/.dotfiles --yes
+
+  break
+done
