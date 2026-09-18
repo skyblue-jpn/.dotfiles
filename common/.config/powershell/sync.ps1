08c9ebb6108ffe10692c5ffddd9aa14f1827ef6d
skyblue-jpn <82032866+skyblue-jpn@users.noreply.github.com>
2026-09-18 13:53:58 +0900

    edit powershell completions dir

--- common/.config/powershell/sync.ps1
+++ common/.config/powershell/sync.ps1
@@ -1,11 +1,11 @@
 $ProfileDir = Split-Path -Parent $PROFILE
 
 # Sync Completions
-if (Test-Path "$ProfileDir\Completions") {
-    Get-ChildItem -Path "$ProfileDir\Completions" -Filter "*.ps1" -Recurse | ForEach-Object { . $_.FullName }
+if (Test-Path "$ProfileDir\completions") {
+    Get-ChildItem -Path "$ProfileDir\completions" -Filter "*.ps1" -Recurse | ForEach-Object { . $_.FullName }
 }
 
 # Sync Functions
-if (Test-Path "$ProfileDir\Functions") {
-    Get-ChildItem -Path "$ProfileDir\Functions" -Filter "*.ps1" -Recurse | ForEach-Object { . $_.FullName }
+if (Test-Path "$ProfileDir\functions") {
+    Get-ChildItem -Path "$ProfileDir\functions" -Filter "*.ps1" -Recurse | ForEach-Object { . $_.FullName }
 }
